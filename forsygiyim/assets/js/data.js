2a44cdfdcc8f479a03f23242a33a31baefe9ce71
Azat Kizilkaya <kizilkayazat@gmail.com>
2026-01-18 23:29:32 +0300

    Add color selection and image gallery to product details

--- forsygiyim/assets/js/data.js
+++ forsygiyim/assets/js/data.js
@@ -4,63 +4,186 @@ const products = [
         name: "Laon Smart Erkek Polo Tişört Siyah",
         price: 279.90,
         category: "T-Shirt",
-        image: "gorseller/6940019b6aa2c-1t.jpg"
-    },
-    {
-        id: 10,
-        name: "Tuna Modern Grafik T- Shirt Beyaz",
-        price: 799.99,
-        category: "T-Shirt",
-        image: "gorseller/6941e3eed42ad-tuna-modern-grafik-t-shirt-beyaz-9128-erkek-basic-tisort-lufian-410567-91-B.jpg"
-    },
-    {
-        id: 11,
-        name: "Sara Erkek Uzun Kol T- Shırt Açık Kahve",
-        price: 999.99,
-        category: "T-Shirt",
-        image: "gorseller/6941e27398711-sara-erkek-uzun-kol-t-shirt-acik-kahve-erkek-bisiklet-yaka-tisort-lufian-404846-96-B.jpg"
-    },
-    {
-        id: 24,
-        name: "Vernon Smart Erkek Polo Tişört Çimen Yeşili",
-        price: 899.99,
-        category: "T-Shirt",
-        image: "gorseller/6941c746eb013-vernon-smart-erkek-polo-tisort-cimen-yesili-erkek-polo-yaka-tisort-lufian-412579-94-B.jpg"
-    },
-    {
-        id: 36,
-        name: "Beli Lastikli Ekoseli Gri Pijama",
-        price: 399.00,
-        category: "Eşofman Altı",
-        image: "gorseller/693f1d0e82314-beli-lastikli-ekoseli-gri-pijama-2d239-.jpg"
-    },
-    {
-        id: 37,
-        name: "Biyeli Eşofman Altı",
-        price: 899.00,
-        category: "Eşofman Altı",
-        image: "gorseller/694921a90e0af-693ebb0fc574a-1.b.jpg"
-    },
-    {
-        id: 38,
-        name: "Basic Ayarlanabilir Paça Bordo Eşofman Altı",
-        price: 455.00,
-        category: "Eşofman Altı",
-        image: "gorseller/693f1a3fce8e4-1.f.jpg"
-    },
-    {
-        id: 39,
-        name: "Ayarlanabilir Paça Basic Yeşil Baggy Eşofman Altı",
-        price: 500.00,
-        category: "Eşofman Altı",
-        image: "gorseller/693eca33a2e7f-1.c.jpg"
+        image: "gorseller/6940019b6aa2c-1t.jpg",
+        colors: [
+            {
+                name: "Siyah",
+                hex: "#000000",
+                images: [
+                    "gorseller/6940019b6aa2c-1t.jpg",
+                    "gorseller/694001a94147d-1t.jpg",
+                    "gorseller/694001b1269a0-1t.jpg",
+                    "gorseller/694001b54287b-3t.jpg"
+                ]
+            },
+            {
+                name: "Kırmızı",
+                hex: "#ff0000",
+                images: [
+                    "gorseller/694001bda74b4-1s.jpg",
+                    "gorseller/694001c1b82ae-1s.jpg",
+                    "gorseller/694001c616c99-2s.jpg"
+                ]
+            }
+        ]
     },
     {
         id: 40,
         name: "Siyah Baggy Eşofman Altı",
         price: 599.99,
         category: "Eşofman Altı",
-        image: "gorseller/693ec5e0197bc-6.a.jpg"
+        image: "gorseller/693ec5e0197bc-6.a.jpg",
+        colors: [
+            {
+                name: "Siyah",
+                hex: "#000000",
+                images: [
+                    "gorseller/693ec5e0197bc-6.a.jpg",
+                    "gorseller/693ebdf5797c4-3.a.jpg",
+                    "gorseller/693ebe11dab4f-4.a.jpg",
+                    "gorseller/693ebe2065b19-5.a.jpg"
+                ]
+            },
+            {
+                name: "Antrasit",
+                hex: "#333333",
+                images: [
+                    "gorseller/693ec59be82a7-5.a.jpg",
+                    "gorseller/693ec5a30f1ed-3.a.jpg",
+                    "gorseller/693ec5a843489-2.a.jpg"
+                ]
+            }
+        ]
+    },
+    {
+        id: 24,
+        name: "Vernon Smart Erkek Polo Tişört Çimen Yeşili",
+        price: 899.99,
+        category: "T-Shirt",
+        image: "gorseller/6941c746eb013-vernon-smart-erkek-polo-tisort-cimen-yesili-erkek-polo-yaka-tisort-lufian-412579-94-B.jpg",
+        colors: [
+            {
+                name: "Yeşil",
+                hex: "#008000",
+                images: [
+                    "gorseller/6941c746eb013-vernon-smart-erkek-polo-tisort-cimen-yesili-erkek-polo-yaka-tisort-lufian-412579-94-B.jpg",
+                    "gorseller/6941c77c9749b-vernon-smart-erkek-polo-tisort-cimen-yesili-erkek-polo-yaka-tisort-lufian-412579-94-B.jpg",
+                    "gorseller/6941c786223b9-vernon-smart-erkek-polo-tisort-cimen-yesili-erkek-polo-yaka-tisort-lufian-412580-94-B.jpg"
+                ]
+            },
+            {
+                name: "Lacivert",
+                hex: "#072d81",
+                images: [
+                    "gorseller/6941c79d0b161-vernon-smart-erkek-polo-tisort-indigo-erkek-polo-yaka-tisort-lufian-412555-94-B.jpg",
+                    "gorseller/6941c7a28c5ab-vernon-smart-erkek-polo-tisort-indigo-erkek-polo-yaka-tisort-lufian-412556-94-B.jpg"
+                ]
+            }
+        ]
+    },
+    {
+        id: 10,
+        name: "Tuna Modern Grafik T- Shirt Beyaz",
+        price: 799.99,
+        category: "T-Shirt",
+        image: "gorseller/6941e3eed42ad-tuna-modern-grafik-t-shirt-beyaz-9128-erkek-basic-tisort-lufian-410567-91-B.jpg",
+        colors: [
+            {
+                name: "Beyaz",
+                hex: "#ffffff",
+                images: [
+                    "gorseller/6941e3eed42ad-tuna-modern-grafik-t-shirt-beyaz-9128-erkek-basic-tisort-lufian-410567-91-B.jpg",
+                    "gorseller/6941e3f7072c5-tuna-modern-grafik-t-shirt-beyaz-9128-erkek-basic-tisort-lufian-410567-91-B.jpg",
+                    "gorseller/6941e40045501-tuna-modern-grafik-t-shirt-beyaz-9128-erkek-basic-tisort-lufian-410568-91-B.jpg"
+                ]
+            }
+        ]
+    },
+    {
+        id: 37,
+        name: "Biyeli Eşofman Altı",
+        price: 899.00,
+        category: "Eşofman Altı",
+        image: "gorseller/694921a90e0af-693ebb0fc574a-1.b.jpg",
+        colors: [
+            {
+                name: "Siyah",
+                hex: "#000000",
+                images: [
+                    "gorseller/694921a90e0af-693ebb0fc574a-1.b.jpg",
+                    "gorseller/693f1c1d8d6bd-biyeli-esofman-alti-5-4902.jpg",
+                ]
+            },
+            {
+                name: "Gri",
+                hex: "#808080",
+                images: [
+                    "gorseller/693f1c8501960-biyeli-esofman-alti-f-475c.jpg",
+                    "gorseller/693f1c8cc9fc7-biyeli-esofman-alti-e-b6a8.jpg"
+                ]
+            }
+        ]
+    },
+    {
+        id: 11,
+        name: "Sara Erkek Uzun Kol T- Shırt Açık Kahve",
+        price: 999.99,
+        category: "T-Shirt",
+        image: "gorseller/6941e27398711-sara-erkek-uzun-kol-t-shirt-acik-kahve-erkek-bisiklet-yaka-tisort-lufian-404846-96-B.jpg",
+        colors: [
+            {
+                name: "Kahve",
+                hex: "#996219",
+                images: ["gorseller/6941e27398711-sara-erkek-uzun-kol-t-shirt-acik-kahve-erkek-bisiklet-yaka-tisort-lufian-404846-96-B.jpg"]
+            },
+            {
+                name: "Siyah",
+                hex: "#000000",
+                images: ["gorseller/6941e2c12b9c0-sara-erkek-uzun-kol-t-shirt-siyah-erkek-basic-tisort-lufian-405338-97-B.jpg"]
+            }
+        ]
+    },
+    {
+        id: 36,
+        name: "Beli Lastikli Ekoseli Gri Pijama",
+        price: 399.00,
+        category: "Eşofman Altı",
+        image: "gorseller/693f1d0e82314-beli-lastikli-ekoseli-gri-pijama-2d239-.jpg",
+        colors: [
+            {
+                name: "Gri",
+                hex: "#808080",
+                images: ["gorseller/693f1d0e82314-beli-lastikli-ekoseli-gri-pijama-2d239-.jpg"]
+            }
+        ]
+    },
+    {
+        id: 38,
+        name: "Basic Ayarlanabilir Paça Bordo Eşofman Altı",
+        price: 455.00,
+        category: "Eşofman Altı",
+        image: "gorseller/693f1a3fce8e4-1.f.jpg",
+        colors: [
+            {
+                name: "Bordo",
+                hex: "#e90003",
+                images: ["gorseller/693f1a3fce8e4-1.f.jpg"]
+            }
+        ]
+    },
+    {
+        id: 39,
+        name: "Ayarlanabilir Paça Basic Yeşil Baggy Eşofman Altı",
+        price: 500.00,
+        category: "Eşofman Altı",
+        image: "gorseller/693eca33a2e7f-1.c.jpg",
+        colors: [
+            {
+                name: "Yeşil",
+                hex: "#008000",
+                images: ["gorseller/693eca33a2e7f-1.c.jpg"]
+            }
+        ]
     }
 ];
 
